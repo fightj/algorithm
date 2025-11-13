bf0d622bc2ded29dcfd1adbd347db966a6c67298
fightj <fightj@naver.com>
2025-11-13 16:40:02 +0900

    최적화 풀이법 추가

--- 25.11.13 2차원배열 색종이 실버5.py
+++ 25.11.13 2차원배열 색종이 실버5.py
@@ -14,3 +14,18 @@ for i in range(100):
             result += 1
 
 print(result)
+
+
+# 보다 더 최적화 하는 방법
+lst = [[False]*100 for _ in range(100)]
+n = int(input())
+
+for _ in range(n):
+    x, y = map(int, input().split())
+    for i in range(10):
+        for j in range(10):
+            lst[x+i][y+j] = True
+
+result = sum(sum(row) for row in lst)
+print(result)
+# true, false로 메모리를 더 아낄 수 있음 그리고 더 빠름
